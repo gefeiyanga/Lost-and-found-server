0eef86a39f39c69fb010d11fa021fd28fe836568
gefeiyanga <variousdid@gmail.com>
2019-05-02 15:29:32 +0800

    by gfy

---
+++ routes/lTrousersList.router.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const router=express.Router();
+const pool=require('../pool');
+router.get('/',(req,res)=>{
+    var sql='select avatar,DATE_FORMAT(date,"%Y-%m-%d %H:%i") AS date,uname,title,detail,tele,name,pic from l_trousers';
+    pool.query(sql,[],(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+})
+
+module.exports=router;
